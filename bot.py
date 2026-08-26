25d1153471b0ccc8847c68570f8c943daea2ec75
ivamaniukova <imaniukova@gmail.com>
2026-08-26 11:21:32 +0200

    Added add_email and add_address and connected methods, adjusted overall structure for consistency

--- bot.py
+++ bot.py
@@ -3,9 +3,11 @@ from record import Record
 from decorators import input_error_handler
 import pickle
 
-valid_commands = ["hello", "close", "exit", "add", "change", "all", "phone", "add-birthday", "show-birthday", "birthdays"]
+valid_commands = ["hello", "close", "exit", "add", "change", "all", "phone", "add_birthday", "show_birthday", "birthdays", "add_email", "edit_email", "get_email", "remove_email", "add_address", "edit_address", "get_address", "remove_address"]
 
 def parse_input(user_input):
+    if not user_input:
+        return "", []
     cmd, *args = user_input.split()
     cmd = cmd.strip().lower()
     return cmd, *args 
@@ -82,9 +84,9 @@ def show_all_contacts(book):
 @input_error_handler
 def add_birthday(book, *args):
     if len(args) < 2:
-        raise ValueError("Insufficient arguments for 'add-birthday' command. Please provide both name and birthday.")
+        raise ValueError("Insufficient arguments for this command. Please provide both name and birthday.")
 
-    name, birthday = args
+    name, birthday, *_ = args
     record = book.find(name)
 
     if record is None:
@@ -97,9 +99,9 @@ def add_birthday(book, *args):
 @input_error_handler
 def show_birthday(book, *args):
     if len(args) < 1:
-        raise ValueError("Insufficient arguments 'show-birthday' command. Please provide the name.")
+        raise ValueError("Insufficient arguments for this command. Please provide the name.")
 
-    name = args[0]
+    name, *_ = args
     record = book.find(name)
 
     if record is None:
@@ -122,6 +124,122 @@ def upcoming_birthdays(book, *args):
         congratulation_date = birthday_info["congratulation_date"]
         print(f"{name}: {congratulation_date}")
 
+@input_error_handler
+def add_email(book, *args):
+    if len(args) < 2:
+        raise ValueError("Insufficient arguments for this command. Please provide both name and email.")
+
+    name, email, *_ = args
+    record = book.find(name)
+
+    if record is None:
+        raise ValueError(f"Contact with name '{name}' does not exist.")
+
+    record.add_email(email)
+
+    print(f"Adding an email: {name} {email}")
+
+@input_error_handler
+def edit_email(book, *args):
+    if len(args) < 2:
+        raise ValueError("Insufficient arguments for this command. Please provide both name and new email.")
+
+    name, new_email, *_ = args
+    record = book.find(name)
+
+    if record is None:
+        raise ValueError(f"Contact with name '{name}' does not exist.")
+
+    record.edit_email(new_email)
+
+    print(f"Editing email: {name} {new_email}")
+
+@input_error_handler
+def get_email(book, *args):
+    if len(args) < 1:
+        raise ValueError("Insufficient arguments for this command. Please provide the name.")
+
+    name, *_ = args
+    record = book.find(name)
+
+    if record is None:
+        raise ValueError(f"Contact with name '{name}' does not exist.")
+
+    email = record.get_email()
+    print(f"Email for {name}: {email}")
+
+@input_error_handler
+def remove_email(book, *args):
+    if len(args) < 1:
+        raise ValueError("Insufficient arguments for this command. Please provide the name.")
+
+    name, *_ = args
+    record = book.find(name)
+
+    if record is None:
+        raise ValueError(f"Contact with name '{name}' does not exist.")
+
+    record.remove_email()
+    print(f"Email removed for {name}.")
+
+@input_error_handler
+def add_address(book, *args):
+    if len(args) < 2:
+        raise ValueError("Insufficient arguments for this command. Please provide both name and address.")
+
+    name, *_ = args
+    address = " ".join(args[1:])
+    record = book.find(name)
+
+    if record is None:
+        raise ValueError(f"Contact with name '{name}' does not exist.")
+
+    record.add_address(address)
+    print(f"Adding an address: {name} {address}")
+
+@input_error_handler
+def edit_address(book, *args):
+    if len(args) < 2:
+        raise ValueError("Insufficient arguments for this command. Please provide both name and new address.")
+
+    name, *_ = args
+    new_address = " ".join(args[1:])
+    record = book.find(name)
+
+    if record is None:
+        raise ValueError(f"Contact with name '{name}' does not exist.")
+
+    record.edit_address(new_address)
+    print(f"Editing address: {name} {new_address}")
+
+@input_error_handler
+def get_address(book, *args):
+    if len(args) < 1:
+        raise ValueError("Insufficient arguments for this command. Please provide the name.")
+
+    name, *_ = args
+    record = book.find(name)
+
+    if record is None:
+        raise ValueError(f"Contact with name '{name}' does not exist.")
+
+    address = record.get_address()
+    print(f"Address for {name}: {address}")
+
+@input_error_handler
+def remove_address(book, *args):
+    if len(args) < 1:
+        raise ValueError("Insufficient arguments for this command. Please provide the name.")
+
+    name, *_ = args
+    record = book.find(name)
+
+    if record is None:
+        raise ValueError(f"Contact with name '{name}' does not exist.")
+
+    record.remove_address()
+    print(f"Address removed for {name}.")
+
 def main():
     book = load_data()
     print("Welcome to the assistant bot!")
@@ -142,14 +260,30 @@ def main():
                 show_phone(book, *args)
             elif command == "all":
                 show_all_contacts(book)
-            elif command == "add-birthday":
+            elif command == "add_birthday":
                 add_birthday(book, *args)
-            elif command == "show-birthday":
+            elif command == "show_birthday":
                 show_birthday(book, *args)
             elif command == "birthdays":
                 upcoming_birthdays(book, *args)
             elif command == "hello":
                 print("How can I help you?")
+            elif command == "add_email":
+                add_email(book, *args)
+            elif command == "edit_email":
+                edit_email(book, *args)
+            elif command == "get_email":
+                get_email(book, *args)
+            elif command == "remove_email":
+                remove_email(book, *args)
+            elif command == "add_address":
+                add_address(book, *args)
+            elif command == "edit_address":
+                edit_address(book, *args)
+            elif command == "get_address":
+                get_address(book, *args)
+            elif command == "remove_address":
+                remove_address(book, *args)
             elif command in ["close", "exit"]:
                 print("Good bye!")
                 save_data(book)
--- record.py
+++ record.py
@@ -1,4 +1,5 @@
 from datetime import datetime as dt
+import re
 
 class Phone:
     def __init__(self, number: str):
@@ -26,20 +27,52 @@ class Birthday:
         except ValueError:
             raise ValueError("Invalid birthday format. Use YYYY-MM-DD.")
 
+class Email:
+    def __init__(self, email: str):
+        self.email = self.validate(email)
+
+    def __str__(self):
+        return self.email
+
+    def validate(self, email: str):
+        email_pattern = r"^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$"
+        if re.match(email_pattern, email):
+            return email
+        raise ValueError("Invalid email format. Use a valid email address (e.g., user@example.com).")
+
+class Address:
+    def __init__(self, address: str):
+        self.address = self.validate(address)
+
+    def __str__(self):
+        return self.address
+
+    def validate(self, address: str):
+        address = address.strip()
+        if address:
+            return address
+        raise ValueError("Address cannot be empty.")
+
 class Record:
     def __init__(self, name: str):
         self.name = name
         self.phones: list[Phone] = []
         self.birthday = None
-
+        self.email = None
+        self.address = None
     def __str__(self):
-        return f"Contact name: {self.name}, Phones: {'; '.join(p.number for p in self.phones)} Birthday: {self.birthday.date if self.birthday else 'N/A'}"
+        return (
+            f"Contact name: {self.name}," 
+            f" Phones: {'; '.join(p.number for p in self.phones)}, " 
+            f"Birthday: {self.birthday.date if self.birthday else 'N/A'}, " 
+            f"Email: {self.email.email if self.email else 'N/A'}, " 
+            f"Address: {self.address.address if self.address else 'N/A'}"
+            )
 
     # Phone methods
     def add_phone(self, phone: str):
         new_phone = Phone(phone)
-        if new_phone.number:
-            self.phones.append(new_phone)
+        self.phones.append(new_phone)
 
     def edit_phone(self, old_number: str, new_number: str):
         phone = self.find_phone(old_number)
@@ -64,10 +97,52 @@ class Record:
     # Birthday methods
     def add_birthday(self, birthday: str):
         new_birthday = Birthday(birthday)
-        if new_birthday.date:
-            self.birthday = new_birthday
+        self.birthday = new_birthday
 
     def get_birthday(self):
         if self.birthday:
             return self.birthday.date
         return "N/A"
+
+    # Email methods
+
+    def add_email(self, email: str):
+        self.email = Email(email)
+
+    def edit_email(self, new_email: str):
+        if self.email is not None:
+            self.email = Email(new_email)
+        else:
+            raise ValueError("No email to edit. Please add an email first.")
+
+    def get_email(self):
+        if self.email is not None:
+            return self.email.email
+        return "N/A"
+
+    def remove_email(self):
+        if self.email is not None:
+            self.email = None
+        else:
+            raise ValueError("No email to remove.")
+
+    # Address methods
+
+    def add_address(self, address: str):
+        self.address = Address(address)
+
+    def edit_address(self, new_address: str):
+        if self.address is not None:
+            self.address = Address(new_address)
+        else:
+            raise ValueError("No address to edit. Please add an address first.")
+    def get_address(self):
+        if self.address is not None:
+            return self.address.address
+        return "N/A"
+
+    def remove_address(self):
+        if self.address is not None:
+            self.address = None
+        else:
+            raise ValueError("No address to remove.")
